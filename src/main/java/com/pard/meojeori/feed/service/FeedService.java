7327f194db649986dab4ab3f8e4f06e71eaf638a
Dohyun Kim <143365353+evryhungry@users.noreply.github.com>
2024-05-25 02:25:20 +0900

    ✅ Add toEntity and timestamp

--- src/main/java/com/pard/meojeori/feed/service/FeedService.java
+++ src/main/java/com/pard/meojeori/feed/service/FeedService.java
@@ -1,9 +1,15 @@
 package com.pard.meojeori.feed.service;
 
+import com.pard.meojeori.feed.dto.FeedDto;
+import com.pard.meojeori.feed.entity.Feed;
+import com.pard.meojeori.feed.repo.FeedRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class FeedService {
+    private final FeedRepo feedRepo;
+
+    public void createFeed(FeedDto.Create dto){ feedRepo.save(Feed.toEntity(dto)); }
 }
