19ae1a4321084cc74fbbab84c4cf29c1235f3bb3
Dohyun Kim <143365353+evryhungry@users.noreply.github.com>
2024-05-25 05:06:04 +0900

    💬 Add swagger commend

--- src/main/java/com/pard/meojeori/feed/controller/FeedController.java
+++ src/main/java/com/pard/meojeori/feed/controller/FeedController.java
@@ -23,26 +23,31 @@ public class FeedController {
         return "게시물 추가 되었습니다.";
     }
     @GetMapping("/detail/{id}")
+    @Operation(summary = "게시물 찾기", description = "글 세부 내용 확인하는 기능")
     public FeedDto.Read findById(@PathVariable("id") Long id) {
         return feedService.findById(id);
     }
 
     @PostMapping("/upvote")
+    @Operation(summary = "좋아요 누르기 기능", description = "글이 마음에 든다면 좋아요를 눌러주세요")
     public void toggleUpvote(@RequestParam Long feedId, @RequestParam UUID userId) {
         feedService.toggleUpvote(feedId, userId);
     }
 
     @GetMapping("/rank/upvote")
+    @Operation(summary = "낭만지수 높은 순 (upvote 내림차순)", description = "낭만지수에 따라서 높은 순으로 랭킹을 나타냄.")
     public List<FeedDto.Read> rankUpvote(){
         return feedService.rankUpvote();
     }
 
     @GetMapping("/rank/price")
+    @Operation(summary = "가격 낮은 순 (price 오름차순)", description = "가격에 따라서 앚은 순으로 랭킹을 나타냄.")
     public List<FeedDto.Read> rankPrice(){
         return feedService.rankPrice();
     }
 
     @GetMapping("/rank/latest")
+    @Operation(summary = "최신 순 (id 내림차순)", description = "가장 먼저 만든 순서에 따라서 게시물을 보여줌")
     public List<FeedDto.Read> rankLatest(){
         return feedService.rankLatest();
     }
