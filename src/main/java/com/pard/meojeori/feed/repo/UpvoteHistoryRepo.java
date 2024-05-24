f13dfc603584eba57caa6285676b4994517e223e
Dohyun Kim <143365353+evryhungry@users.noreply.github.com>
2024-05-25 05:38:05 +0900

    🐛 fix the delete method - Feed

--- src/main/java/com/pard/meojeori/feed/repo/UpvoteHistoryRepo.java
+++ src/main/java/com/pard/meojeori/feed/repo/UpvoteHistoryRepo.java
@@ -11,4 +11,5 @@ import java.util.Optional;
 @Repository
 public interface UpvoteHistoryRepo extends JpaRepository<UpvoteHistory,Long> {
     Optional<UpvoteHistory> findByFeedAndUser(Feed feed, User user);
+    void deleteByFeedId(Long feedId);
 }
