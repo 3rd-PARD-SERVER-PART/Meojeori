7ae006a45b660427de28813e030dd6d9093b330b
Dohyun Kim <143365353+evryhungry@users.noreply.github.com>
2024-05-25 06:46:44 +0900

    🔧 add locahost in CorsConfig

--- src/main/java/com/pard/meojeori/config/CorsConfig.java
+++ src/main/java/com/pard/meojeori/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
                 new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true); //frontEnd에서 axios로 처리 가능하게 만들겠다
-        config.addAllowedOrigin("*"); //모든 ip에 응답을 허용하겠다
+        config.addAllowedOrigin("http://localhost:3000"); //모든 ip에 응답을 허용하겠다
         config.addAllowedHeader("*"); //모든 header에 응답을 허용하겠다
         config.addAllowedMethod("*"); //모든 post,get,put,delete,patch 요청을 허용하겠다
         source.registerCorsConfiguration("*", config); //api로 들어오는 모든 요청은 이 config를 따르겠다
